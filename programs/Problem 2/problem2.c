b0b0e649ae233a3f46c51e0d17d6f92fa6090673
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-24 21:35:45 +0530

    First test
    problem 1 --> sum of +ve and -ve
    problem 2 --> generate primes between interval
    problem 3 --> ratio of 0 to 1 in array

---
+++ programs/Problem 2/problem2.c
@@ -0,0 +1,8 @@
+﻿#include<stdio.h>
+int main()
+{
+	int low,high;//THE RANGE IS STORED IN low AND high variables
+	scanf("%d %d",&low,&high);
+	//START WRITING YOUR CODE FROM HERE
+	return 0;
+}
---
+++ programs/Problem 2/problem2.py
@@ -0,0 +1,5 @@
+#LENGTH OF THE ARRAY IS STORED HERE IN length VARIABLE
+length=int(input())
+#ARRAY ELEMENTS ARE STORED IN arr
+arr=list(map(str,input().split()))
+#START WRITING YOUR CODE FROM HERE
