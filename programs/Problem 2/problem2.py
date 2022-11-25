598baecbfbb6da5aa372ffb5559ebf44cadaa874
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-25 07:13:48 +0530

    Delete programs directory

--- programs/Problem 2/problem2.py
+++
@@ -1,5 +0,0 @@
-#LENGTH OF THE ARRAY IS STORED HERE IN length VARIABLE
-length=int(input())
-#ARRAY ELEMENTS ARE STORED IN arr
-arr=list(map(str,input().split()))
-#START WRITING YOUR CODE FROM HERE
