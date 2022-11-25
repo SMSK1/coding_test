5d3f2a54e73d8c35b5a7bc8786cd22ed95fc84dd
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-25 22:19:03 +0530

    First test

---
+++ programs/Problem 2/Problem2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Problem2
+{
+	public static void main(String[] args)
+	{
+		int low,high;//THE RANGE IS STORED IN low AND high variables
+		Scanner sc =new Scanner(System.in);
+		low=sc.nextInt();
+		high=sc.nextInt();
+		//START WRITING YOUR CODE FROM HERE
+	}
+}
