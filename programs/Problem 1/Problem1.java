9b475ecbbd007b45f91606c70d83002ef874cba6
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-25 07:16:10 +0530

    First Test

---
+++ programs/Problem 1/Problem1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Problem1
+{
+	public static void main(String[] args)
+	{
+		int length; // LENGTH OF THE ARRAY IS STORED HERE IN length VARIABLE
+		Scanner sc =new Scanner(System.in);
+		length=sc.nextInt();
+		int arr[]=new int[length]; // ARRAY ELEMENTS ARE STORED IN arr
+		for(int i=0;i<length;i++)
+			arr[i]=sc.nextInt();
+		//START WRITING YOUR CODE FROM HERE
+	}
+}
+
+
+
+
+
+
+
+
---
+++ programs/Problem 3/Problem3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Problem3
+{
+	public static void main(String[] args)
+	{
+		int length; // LENGTH OF THE ARRAY IS STORED HERE IN length VARIABLE
+		Scanner sc =new Scanner(System.in);
+		length=sc.nextInt();
+		int arr[]=new int[length]; // ARRAY ELEMENTS ARE STORED IN arr
+		for(int i=0;i<length;i++)
+			arr[i]=sc.nextInt();
+		//START WRITING YOUR CODE FROM HERE
+	}
+}
