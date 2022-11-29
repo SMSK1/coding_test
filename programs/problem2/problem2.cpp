328f418d77658fe7635470c1377c805eefea64f9
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-29 22:08:08 +0530

    First Coding Test

---
+++ programs/problem2/problem2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int low,high;//THE RANGE IS STORED IN low AND high variables
+	cin>>low>>high;
+	//START WRITING YOUR CODE FROM HERE
+	return 0;
+}
---
+++ programs/problem3/problem3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int length;// LENGTH OF THE ARRAY IS STORED HERE IN length VARIABLE
+	scanf("%d",&length);	
+	int arr[length]; // ARRAY ELEMENTS ARE STORED IN arr
+	char tmp;
+	for(int i=0;i<length;i++)
+		cin>>arr[i];
+    	//START WRITING YOUR CODE FROM HERE
+	return 0;
+}
