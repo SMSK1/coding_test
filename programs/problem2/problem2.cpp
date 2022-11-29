8189cf1e3d5a8953c8ffeb1c23319ad80a351377
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-29 22:12:10 +0530

    Delete programs directory

--- programs/problem2/problem2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int low,high;//THE RANGE IS STORED IN low AND high variables
-	cin>>low>>high;
-	//START WRITING YOUR CODE FROM HERE
-	return 0;
-}
