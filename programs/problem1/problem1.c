764c31a81a4e9e8c6c10a32a01027eddf6e9fcc3
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-29 22:02:42 +0530

    First Coding Test

---
+++ programs/problem1/problem1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int length;// LENGTH OF THE ARRAY IS STORED IN length
+	scanf("%d",&length);
+	int arr[length]; // ARRAY ELEMENTS ARE STORED IN arr
+	for(int i=0;i<length;i++)
+      	scanf("%d",&arr[i]);
+	//START WRITING YOUR CODE FROM HERE
+	return 0;
+}
