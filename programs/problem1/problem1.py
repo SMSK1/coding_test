b9bebc1d9ffbe04f8072f07ec0e9d959181fbcb4
SMSK1 <118947840+SMSK1@users.noreply.github.com>
2022-11-29 22:01:45 +0530

    Delete programs directory

--- programs/problem1/problem1.py
+++
@@ -1,5 +0,0 @@
-# LENGTH OF THE ARRAY IS STORED HERE IN length VARIABLE
-length=int(input())
-# ARRAY ELEMENTS ARE STORED IN arr
-arr=list(map(int,input().split()))
-#START WRITING YOUR CODE FROM HERE
--- programs/problem2/problem2.c
+++
@@ -1,8 +0,0 @@
-﻿#include<stdio.h>
-int main()
-{
-	int low,high;//THE RANGE IS STORED IN low AND high variables
-	scanf("%d %d",&low,&high);
-	//START WRITING YOUR CODE FROM HERE
-	return 0;
-}
